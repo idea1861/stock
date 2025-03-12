d989220cc9acadc61ddab12f06bea7c37f009b18
idea1861 <idea1861@163.com>
2025-03-12 09:56:30 +0800

    lhc -20250312

--- src/aa.java
+++ src/aa.java
@@ -1,2 +1,16 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.sql.SQLOutput;
+
 public class aa {
+
+    public static void rn(){
+        System.out.println("rrrrrrrr");
+    }
+
+    public static void main(String[] args) {
+        rn();
+    }
+
+
 }
